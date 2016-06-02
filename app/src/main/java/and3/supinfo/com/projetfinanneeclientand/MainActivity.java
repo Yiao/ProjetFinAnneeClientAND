8ba9da293cbe370384f48bdcddfef34cb0053739
yiao <yiao.sun@supinfo.com>
2016-06-02 12:18:42 +0200

    Changement

--- app/src/main/java/and3/supinfo/com/projetfinanneeclientand/MainActivity.java
+++ app/src/main/java/and3/supinfo/com/projetfinanneeclientand/MainActivity.java
@@ -81,16 +81,22 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
+
         } else if (id == R.id.nav_slideshow) {
+
 
         } else if (id == R.id.nav_manage) {
 
+
         } else if (id == R.id.nav_share) {
 
+
         } else if (id == R.id.nav_send) {
+
 
         }
 
